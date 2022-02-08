c58a73d374399646ac6b0af0a675983924769fcd
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-02-08 15:49:31 +0100

    Set up SCSS styles
    Vite supports them automatically, we just need to add the right package
    to our dev dependencies. My npm also decided to sort the dependencies at
    the same time, which I think we might as well keep.
    
    Also, remove the label styles, there’s no label in the HelloWorld
    component (I guess it must’ve been in the original template but was
    removed before the initial commit of the Vite setup).
    
    Bug: T300891

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,20 +29,17 @@ const count = ref( 0 );
 	</p>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 a {
 	color: #42b983;
 }
 
-label {
-	margin: 0 0.5em;
-	font-weight: bold;
-}
-
-code {
-	background-color: #eee;
-	padding: 2px 4px;
-	border-radius: 4px;
-	color: #304455;
+p {
+	code {
+		background-color: #eee;
+		padding: 2px 4px;
+		border-radius: 4px;
+		color: #304455;
+	}
 }
 </style>
