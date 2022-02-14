8d6af451e90ac4a011a7fa69cdda036e5b25ea29
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-02-14 11:40:44 +0100

    Add unit test for main.ts (#27)
    And move the HelloWorld test below components/ for consistency with the
    structure in src/. (Though I guess we’ll throw it away soon anyways.)

---
+++ tests/unit/main.test.ts
@@ -0,0 +1,18 @@
+import createAndMount from '@/main';
+
+describe( 'createAndMount', () => {
+
+	it( 'mounts created app on given selector', () => {
+		const rootElement = document.createElement( 'div' );
+		rootElement.id = 'test-app';
+		document.body.append( rootElement );
+		const discardedElement = document.createElement( 'div' );
+		rootElement.append( discardedElement );
+
+		const instance = createAndMount( { rootSelector: '#test-app' } );
+
+		expect( rootElement.firstChild ).toBe( instance.$el );
+		expect( discardedElement.parentElement ).toBe( null );
+	} );
+
+} );
