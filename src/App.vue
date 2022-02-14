202cd67de592956784f6c23e60616409de531d9a
Itamar Givon <itamar.givon@wikimedia.de>
2022-02-14 14:58:32 +0100

    Remove todo list

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,6 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import NewLexemeForm from '@/components/NewLexemeForm.vue';
 import '@wmde/wikit-vue-components/dist/wikit-vue-components.css';
-
-/**
- * TODO:
- * x Add a form SFC in component's directory
- * - Add i18n keys for submit button
- * - Style form margins, paddings, borders
- */
 </script>
 
 <template>
