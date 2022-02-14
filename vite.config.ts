wmde/new-lexeme-special-page

commit 171d33f321cc0c6a4837292e37e9970a907b127f

Update build outputs for use in WikibaseLexeme (#19)
Use the common.js format with a predicatable file name (no hash), and
externalize both vue and @vue/compat. (We didn’t find a way yet to map
@vue/compat back to vue for the output.) Make main.ts export a function
that takes the selector as part of a config object, and call that
function in the index.html (WikibaseLexeme will call it differently).
Since this means we can no longer style #app, remove the App <style>
block, we didn’t really want those particular styles anyways.

Bug: T300882
Co-Authored-By: Michael Große <michael.grosse@wikimedia.de>

vite.config.ts
@@ -6,6 +6,18 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig( {
+	build: {
+		target: 'es2015',
+		lib: {
+			entry: resolve( __dirname, 'src/main.ts' ),
+			name: 'main',
+			fileName: ( format ) => `SpecialNewLexeme.${format}.js`,
+			formats: [ 'cjs' ],
+		},
+		rollupOptions: {
+			external: [ '@vue/compat', 'vue' ],
+		},
+	},
 	resolve: {
 		alias: {
 			'vue': '@vue/compat',

index.html
@@ -7,6 +7,10 @@
 	</head>
 	<body>
 		<div id="app"></div>
-		<script type="module" src="/src/main.ts"></script>
+		<script type="module">
+			import createAndMount from './src/main.ts';
+
+			createAndMount( { rootSelector: '#app' } );
+		</script>
 	</body>
 </html>

src/App.vue
@@ -9,14 +9,3 @@ import '@wmde/wikit-vue-components/dist/wikit-vue-components.css';
 <template>
 	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
-
-<style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-block-start: 60px;
-}
-</style>

src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from 'vue';
+import {
+	ComponentPublicInstance,
+	createApp,
+} from 'vue';
 import App from './App.vue';
 
-createApp( App ).mount( '#app' );
+interface Config {
+	rootSelector: string;
+}
+
+export default function createAndMount( config: Config ): ComponentPublicInstance {
+	return createApp( App )
+		.mount( config.rootSelector );
+}

commit ec2d4033bb85fd72ae4b8349f88d7a42961191b4

fix Netlify hosting
Netlify actually needs an index.html in the dist directory. However this
is not added for a library build. So this creates an extra build command
to keep building it as an app that makes still use of the index.html,
link `npm run dev`. This skips the type checking as the build currently
runs on Netlify itself and there we only have a limited amount of build
minutes included. Rather the type checking should be part of the "code
quality" CI job.

This Netlify hosting was broken in #19 / 171d33f

vite.config.ts
@@ -1,12 +1,17 @@
 /* eslint quote-props: [ "error", "consistent-as-needed" ] */
 
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { BuildOptions, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-export default defineConfig( {
-	build: {
+function getBuildConfig( isAappBuild: bool ): BuildOptions {
+	if ( isAappBuild ) {
+		return {
+			target: 'es2015',
+		};
+	}
+
+	return {
 		target: 'es2015',
 		lib: {
 			entry: resolve( __dirname, 'src/main.ts' ),
@@ -17,7 +22,12 @@ export default defineConfig( {
 		rollupOptions: {
 			external: [ '@vue/compat', 'vue', 'vuex' ],
 		},
-	},
+	};
+}
+
+// https://vitejs.dev/config/
+export default defineConfig( {
+	build: getBuildConfig( !!process.env.BUILD_AS_APP ),
 	resolve: {
 		alias: {
 			'vue': '@vue/compat',