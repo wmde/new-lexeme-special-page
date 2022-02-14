18d33af81ac0b73969280a1ecca5b6980dd0d39e
Itamar Givon <itamar.givon@wikimedia.de>
2022-02-14 13:03:06 +0100

    Add Basic form component
    Co-authored-by: Noa Rave <chukarave@users.noreply.github.com>

---
+++ src/components/NewLexemeForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button as WikitButton } from '@wmde/wikit-vue-components';
+</script>
+
+<template>
+	<form class="new-lexeme-form">
+		<wikit-button
+			class="form-button-submit"
+			type="progressive"
+			variant="primary"
+		>
+			Create Lexeme
+		</wikit-button>
+	</form>
+</template>
+
+<style scoped lang="scss">
+// FIXME: Remove this ignore rule, added to appease stylelint
+/* stylelint-disable-line no-empty-source */
+</style>
