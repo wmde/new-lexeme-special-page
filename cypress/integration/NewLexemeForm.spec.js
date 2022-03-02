e0c53b713d97eb17a7308861355692babbd63f7d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-03-02 12:16:01 +0100

    Add lemma language input and submit form (#55)
    This is a temporary solution to make creating lexemes work, by targeting
    the noscript backend of the special page; we want to replace this with
    an API request later. (At some point the hard-coded lemma-language will
    also be replaced, of course.)
    
    The previous simple Cypress test is replaced with one that watches for a
    form submit and checks the submitted data (including a token that we
    hard-code in index.html). Unfortunately, Chromium reports inexplicable
    cross-origin errors when submitting, blocking the iframe with origin
    http://localhost:3000 from accessing a cross-origin frame at
    http://localhost:3000, or something like that (yes, that’s the same host
    twice, as far as I can tell). I haven’t been able to find a solution for
    that, so for now, work around it by disabling Chrome web security in
    cypress.json. We can remove that line once we no longer submit the form.
    
    Bug: T298153

--- cypress/integration/NewLexemeForm.spec.js
+++ cypress/integration/NewLexemeForm.spec.js
@@ -1,11 +1,32 @@
 describe( 'NewLexemeForm', () => {
 
-	it( 'adds a hidden input with the CSRF token', () => {
+	it( 'submits form data', () => {
 		cy.visit( '/' );
 
-		cy.get( 'input[name=wpEditToken]' )
-			.should( 'have.value', 'dev-token' )
-			.should( 'be.hidden' );
+		cy.intercept( 'POST', '/' )
+			.as( 'post' );
+
+		cy.get( 'input[name=lemma]' )
+			.type( 'test lemma' );
+
+		cy.get( 'input[name=lexeme-language]' )
+			.type( 'Q123' );
+
+		cy.get( 'input[name=lexicalcategory]' )
+			.type( 'Q456' );
+
+		cy.get( '.wbl-snl-form' )
+			.submit();
+
+		cy.wait( '@post' )
+			.then( ( { request } ) => {
+				const params = new URLSearchParams( request.body );
+				expect( params.get( 'lemma' ) ).to.equal( 'test lemma' );
+				expect( params.get( 'lemma-language' ) ).to.equal( 'en' );
+				expect( params.get( 'lexeme-language' ) ).to.equal( 'Q123' );
+				expect( params.get( 'lexicalcategory' ) ).to.equal( 'Q456' );
+				expect( params.get( 'wpEditToken' ) ).to.equal( 'dev-token' );
+			} );
 	} );
 
 } );
