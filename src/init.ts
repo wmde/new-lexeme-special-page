e7bec9d9cbc28f2590efcb6b6f0f103ad3b43c81
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-03-11 15:38:53 +0100

    Turn second createAndMount() param into services object (#81)
    We’ll need more than one service soon, so let’s turn this into a
    container of possibly several services. The messagesRepository service
    remains optional for now, but the whole services object is required,
    even if it’s just the empty object. (I’m not yet sure if additional
    services will also be optional or required.)
    
    Bug: T302961

--- src/init.ts
+++ src/init.ts
@@ -1,10 +1,16 @@
-import createAndMount, { Config } from './main';
+import createAndMount, {
+	Config,
+	Services,
+} from './main';
 import MwMessagesRepository from './mediawiki/MwMessagesRepository';
 import { MediaWiki } from './@types/mediawiki';
 import { ComponentPublicInstance } from 'vue';
 
 export default function init( config: Config, mw: MediaWiki ): ComponentPublicInstance {
 	const messagesRepository = new MwMessagesRepository( mw.message );
+	const services: Services = {
+		messagesRepository,
+	};
 
-	return createAndMount( config, messagesRepository );
+	return createAndMount( config, services );
 }
