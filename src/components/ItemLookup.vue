de9a8eb99e3b85229341de2c3431805cbd7cc788
Michael Große <michael.grosse@wikimedia.de>
2022-03-16 13:39:12 +0100

    Add search prop to ItemLookup

--- src/components/ItemLookup.vue
+++ src/components/ItemLookup.vue
@@ -27,7 +27,7 @@ interface Props {
 	label: string;
 	placeholder: string;
 	value: string | null;
-	searchForItems: ( searchTerm: string, offset?: number ) => SearchedItemOption[];
+	searchForItems: ( searchTerm: string, offset?: number ) => Promise<SearchedItemOption[]>;
 	// error?: 'ERROR_LEMMA_TOO_LONG' | 'ERROR_NO_LEMMA' | null;
 }
 const props = defineProps<Props>();
@@ -37,79 +37,33 @@ const emit = defineEmits( {
 	},
 } );
 
-const vegetableItems: MonolingualItemOption[] = [
-	{
-		label: 'potato',
-		description: 'root vegetable',
-		value: 'Q10998',
-	},
-	{
-		label: 'carrot',
-		description: 'root vegetable, usually orange in color',
-		value: 'Q81',
-		tag: 'limited support',
-	},
-	{
-		label: 'zucchini',
-		description: 'Edible summer squash, typically green in colour',
-		value: 'Q7533',
-	},
-	{
-		label: 'eggplant',
-		description: 'plant species Solanum melongena',
-		value: 'Q7540',
-	},
-	{
-		label: 'broccoli',
-		description: 'edible green plant in the cabbage family',
-		value: 'Q47722',
-	},
-	{
-		label: 'cauliflower',
-		description: 'vegetable, for the plant see Q7537 (Brassica oleracea var. botrytis)',
-		value: 'Q23900272',
-	},
-	{
-		label: 'brussels sprouts',
-		description: 'vegetable',
-		value: 'Q150463',
-	},
-	{
-		label: 'cassava root',
-		description: 'root vegetable',
-		value: 'Q43304555',
-	},
-	{
-		label: 'plantain',
-		description: 'banana-like vegetable, less sweet',
-		value: 'Q165449',
-	},
-	{
-		label: 'cabbage',
-		description: 'Vegetable, the generic term for several varieties.',
-		value: 'Q14328596',
-	},
-	{
-		label: 'napa cabbage',
-		description: 'a type of Chinese cabbage',
-		value: 'Q13360268',
-	},
-];
-
-const searchResults = ref( [] as MonolingualItemOption[] );
-
-searchResults.value = vegetableItems;
+const searchSuggestions = ref( [] as MonolingualItemOption[] );
 
 const searchInput = ref( '' );
-const onSearchInput = ( value: string ) => {
-	console.log( 'search input 2: ' + value );
-	searchInput.value = value;
-	searchResults.value = vegetableItems.filter( ( item ) => item.label.includes( value ) );
-	// FIXME: search for items here!
+const onSearchInput = async ( inputValue: string ) => {
+	searchInput.value = inputValue;
+	const searchResults = await props.searchForItems( inputValue );
+	searchSuggestions.value = searchResults.map( ( result ) => {
+		return {
+			label: result.display.label.value,
+			description: result.display.description.value,
+			value: result.itemId,
+		};
+	} );
 };
 
-const onScroll = () => {
-	console.log( 'list of results is being scrolled - load more!' );
+const onScroll = async () => {
+	const searchReults = await props.searchForItems(
+		searchInput.value,
+		searchSuggestions.value.length,
+	);
+	searchSuggestions.value.push( ...searchReults.map( ( result ) => {
+		return {
+			label: result.display.label.value,
+			description: result.display.description.value,
+			value: result.itemId,
+		};
+	} ) );
 };
 
 const onOptionSelected = ( value: unknown ) => {
@@ -121,7 +75,7 @@ const selectedOption = computed( () => {
 	if ( props.value === null ) {
 		return null;
 	}
-	return searchResults.value.find( ( item ) => item.value === props.value );
+	return searchSuggestions.value.find( ( item ) => item.value === props.value );
 } );
 
 </script>
@@ -131,7 +85,7 @@ const selectedOption = computed( () => {
 		:label="label"
 		:placeholder="placeholder"
 		:search-input="searchInput"
-		:menu-items="searchResults"
+		:menu-items="searchSuggestions"
 		:value="selectedOption"
 		@update:search-input="onSearchInput"
 		@scroll="onScroll"
--- src/components/LanguageInput.vue
+++ src/components/LanguageInput.vue
@@ -11,6 +11,30 @@ defineProps<Props>();
 defineEmits( [ 'update:modelValue' ] );
 
 const messages = useMessages();
+
+// ! The following method was entirely written by CoPilot! (except for `origin=*`)
+const searchForItems = async ( searchTerm: string, offset: number = 0 ) => {
+	const searchResults = await fetch(
+		`https://www.wikidata.org/w/api.php?action=wbsearchentities&search=${searchTerm}&language=en&format=json&type=item&limit=10&continue=${offset}&origin=*`,
+	);
+	const searchResultsJson = await searchResults.json();
+	return searchResultsJson.search.map( ( result ) => {
+		return {
+			display: {
+				label: {
+					language: 'en',
+					value: result.label,
+				},
+				description: {
+					language: 'en',
+					value: result.description,
+				},
+			},
+			itemId: result.id,
+		};
+	} );
+};
+
 </script>
 
 <script lang="ts">
@@ -27,6 +51,7 @@ export default {
 			:label="messages.get( 'wikibaselexeme-newlexeme-language' )"
 			:placeholder="messages.get( 'wikibaselexeme-newlexeme-language-placeholder' )"
 			:value="modelValue"
+			:search-for-items="searchForItems"
 			@update:model-value="$emit( 'update:modelValue', $event )"
 		/>
 	</div>
