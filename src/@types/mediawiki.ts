83a2256ebcdd0259ec51de99b125b55406861e26
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-03-16 12:28:34 +0100

    Introduce LexemeCreator with API-based implementation
    It’s not wired up anywhere yet. The alert-based implementation is
    intended to be used in the dev entry point (index.html).
    
    Bug: T302961
    Co-Authored-By: Michael Große <michael.grosse@wikimedia.de>
    Co-Authored-By: Noa Rave <noa.rave@wikimedia.de>

--- src/@types/mediawiki.ts
+++ src/@types/mediawiki.ts
@@ -4,6 +4,12 @@ export interface MwMessage {
 }
 export type MwMessages = ( key: string, ...params: readonly ( string|HTMLElement )[] ) => MwMessage;
 
+export interface MwApi {
+	assertCurrentUser( params: object ): object;
+	postWithEditToken( params: object ): Promise<unknown>;
+}
+
 export interface MediaWiki {
 	message: MwMessages;
+	Api: new() => MwApi;
 }
---
+++ tests/unit/mediawiki/MwApiLexemeCreator.test.ts
@@ -0,0 +1,39 @@
+import MwApiLexemeCreator from '@/mediawiki/MwApiLexemeCreator';
+
+describe( 'MwApiLexemeCreator', () => {
+
+	it( 'POSTs the right data', async () => {
+		const api = {
+			assertCurrentUser: ( params: object ) => ( { assert: 'user', ...params } ),
+			postWithEditToken: jest.fn().mockResolvedValue( { entity: { id: 'L123' } } ),
+		};
+		const lexemeCreator = new MwApiLexemeCreator( api, [ 'tag' ] );
+
+		const id = await lexemeCreator.createLexeme(
+			'lemma',
+			'en',
+			'Q123',
+			'Q456',
+		);
+
+		expect( id ).toBe( 'L123' );
+		expect( api.postWithEditToken ).toHaveBeenCalledTimes( 1 );
+		expect( api.postWithEditToken ).toHaveBeenCalledWith( {
+			action: 'wbeditentity',
+			new: 'lexeme',
+			tags: [ 'tag' ],
+			data: JSON.stringify( {
+				lemmas: {
+					en: {
+						language: 'en',
+						value: 'lemma',
+					},
+				},
+				language: 'Q123',
+				lexicalCategory: 'Q456',
+			} ),
+			assert: 'user',
+		} );
+	} );
+
+} );
