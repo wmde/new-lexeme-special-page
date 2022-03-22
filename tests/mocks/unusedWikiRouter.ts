3858236ecaf606908a81dddeedf4216034e37950
Michael Große <mic.grosse@googlemail.com>
2022-03-22 19:29:06 +0100

    Use correct mock method to throw error

--- tests/mocks/unusedWikiRouter.ts
+++ tests/mocks/unusedWikiRouter.ts
@@ -1,7 +1,7 @@
 import WikiRouter from '@/plugins/WikiRouterPlugin/WikiRouter';
 
 const unusedWikiRouter: WikiRouter = {
-	goToTitle: jest.fn().mockRejectedValue( new Error( 'WikiRouter should not be used in this test' ) ),
+	goToTitle: jest.fn().mockImplementation( () => { throw new Error( 'WikiRouter should not be used in this test' ); } ),
 };
 
 export default unusedWikiRouter;
