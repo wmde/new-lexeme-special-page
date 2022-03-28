d0d9238efa5355f40d9785c93ec3d976d4044c8c
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2022-03-28 12:17:41 +0200

    Add wikibaseLexemeTermLanguages to Config interface.
    Bug: T304496

--- src/plugins/ConfigPlugin/Config.ts
+++ src/plugins/ConfigPlugin/Config.ts
@@ -6,6 +6,7 @@ import {
 export interface Config {
 	readonly licenseUrl: string;
 	readonly licenseName: string;
+	readonly wikibaseLexemeTermLanguages: string[];
 }
 
 export const ConfigKey: InjectionKey<Config> = Symbol( 'Config' );
