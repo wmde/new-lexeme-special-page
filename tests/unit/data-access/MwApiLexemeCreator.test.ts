6f41a72e82f9e31711ac8bcb0a611cd22136104b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-03-29 12:52:44 +0200

    Make MwApiLexemeCreator return API errors
    If there are proper HTML errors in the API response, return those so we
    can show them (at least the first one) in NewLexemeForm. If there aren’t
    any errors, use the mw.Api()-generated code instead of hard-coding
    “unknown” (it will probably be “http” in case of a network error, or
    possibly “ok-but-empty” or “unknown”).
    
    Bug: T304489

--- tests/unit/data-access/MwApiLexemeCreator.test.ts
+++ tests/unit/data-access/MwApiLexemeCreator.test.ts
@@ -1,6 +1,8 @@
 import { MwApi } from '@/@types/mediawiki';
 import MwApiLexemeCreator from '@/data-access/MwApiLexemeCreator';
+import { SubmitError } from '@/store/RootState';
 import unusedApi from '../../mocks/unusedApi';
+import $ from 'jquery';
 
 describe( 'MwApiLexemeCreator', () => {
 
@@ -36,7 +38,64 @@ describe( 'MwApiLexemeCreator', () => {
 				lexicalCategory: 'Q456',
 			} ),
 			assert: 'user',
+			errorformat: 'html',
+			formatversion: 2,
 		} );
+	} );
+
+	describe( 'error handling', () => {
+
+		it( 'maps API errors', () => {
+			const deferred = $.Deferred();
+			const result = { errors: [
+				{ code: 'error1', html: 'error one' },
+				{ code: 'error2', html: 'error two' },
+				{ code: 'error3' },
+			] };
+			deferred.reject( 'error1', result, result );
+			const api: MwApi = {
+				...unusedApi,
+				assertCurrentUser: ( params: object ) => ( { assert: 'user', ...params } ),
+				postWithEditToken: jest.fn().mockReturnValue( deferred ),
+			};
+			const lexemeCreator = new MwApiLexemeCreator( api );
+
+			return expect( () => lexemeCreator.createLexeme(
+				'lemma',
+				'en',
+				'Q123',
+				'Q456',
+			) ).rejects.toStrictEqual( [
+				{ type: 'error1', message: 'error one' },
+				{ type: 'error2', message: 'error two' },
+				{ type: 'error3' },
+			] as SubmitError[] );
+		} );
+
+		it( 'handles HTTP errors', () => {
+			const deferred = $.Deferred();
+			deferred.reject( 'http', {
+				xhr: undefined,
+				textStatus: undefined,
+				exception: undefined,
+			} );
+			const api: MwApi = {
+				...unusedApi,
+				assertCurrentUser: ( params: object ) => ( { assert: 'user', ...params } ),
+				postWithEditToken: jest.fn().mockReturnValue( deferred ),
+			};
+			const lexemeCreator = new MwApiLexemeCreator( api );
+
+			return expect( () => lexemeCreator.createLexeme(
+				'lemma',
+				'en',
+				'Q123',
+				'Q456',
+			) ).rejects.toStrictEqual( [
+				{ type: 'http' },
+			] as SubmitError[] );
+		} );
+
 	} );
 
 } );
