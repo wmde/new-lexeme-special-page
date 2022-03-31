a1827d3b8addd05a912fea1c58c592e0f2d0198b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-03-31 14:34:53 +0200

    Make MwApiLexemeCreator return API errors (#103)
    If there are proper HTML errors in the API response, return those so we
    can show them (at least the first one) in NewLexemeForm. If there aren’t
    any errors, use the mw.Api()-generated code instead of hard-coding
    “unknown” (it will probably be “http” in case of a network error, or
    possibly “ok-but-empty” or “unknown”).
    
    Bug: T304489

--- src/data-access/MwApiLexemeCreator.ts
+++ src/data-access/MwApiLexemeCreator.ts
@@ -1,10 +1,18 @@
 import { MwApi } from '@/@types/mediawiki';
 import LexemeCreator from '@/data-access/LexemeCreator';
+import { SubmitError } from '@/store/RootState';
 
 type WbEditEntityResponse = {
 	entity: {
 		id: string;
 	};
+};
+
+type ApiResult = {
+	errors: {
+		code: string;
+		html: string;
+	}[];
 };
 
 export default class MwApiLexemeCreator implements LexemeCreator {
@@ -37,10 +45,32 @@ export default class MwApiLexemeCreator implements LexemeCreator {
 				language: lexemeLanguageItemId,
 				lexicalCategory: lexicalCategoryItemId,
 			} ),
+			errorformat: 'html',
+			formatversion: 2,
 		} );
 		const response = await this.api.postWithEditToken( params )
-			.catch( () => {
-				return Promise.reject( [ { type: 'unknown' } ] );
+			// jQuery promises can resolve and reject with multiple values;
+			// the third argument is either missing or the result
+			.catch( ( code: string, _?: unknown, result?: ApiResult ): Promise<never> => {
+				let errors: SubmitError[];
+				try {
+					if ( result && result.errors ) {
+						errors = result.errors.map( ( apiError ) => {
+							const submitError: SubmitError = { type: apiError.code };
+							if ( apiError.html ) {
+								submitError.message = apiError.html;
+							}
+							return submitError;
+						} );
+					} else {
+						errors = [ { type: code } ];
+					}
+				} catch ( e ) {
+					// eslint-disable-next-line no-console
+					console.error( 'Unexpected API result', result, e );
+					errors = [ { type: 'assertionerror' } ];
+				}
+				return Promise.reject( errors );
 			} );
 
 		return ( response as WbEditEntityResponse ).entity.id;
--- tests/unit/data-access/MwApiLexemeCreator.test.ts
+++ tests/unit/data-access/MwApiLexemeCreator.test.ts
@@ -1,6 +1,16 @@
 import { MwApi } from '@/@types/mediawiki';
 import MwApiLexemeCreator from '@/data-access/MwApiLexemeCreator';
+import { SubmitError } from '@/store/RootState';
 import unusedApi from '../../mocks/unusedApi';
+
+/* Return an object that works a bit like a jQuery.Deferred() rejected with the given args. */
+function mockRejectedDeferred( ...args: unknown[] ): unknown {
+	return {
+		catch<T>( callback: ( ..._: unknown[] ) => PromiseLike<T> ): Promise<T> {
+			return Promise.resolve( callback( ...args ) );
+		},
+	};
+}
 
 describe( 'MwApiLexemeCreator', () => {
 
@@ -36,7 +46,62 @@ describe( 'MwApiLexemeCreator', () => {
 				lexicalCategory: 'Q456',
 			} ),
 			assert: 'user',
+			errorformat: 'html',
+			formatversion: 2,
 		} );
+	} );
+
+	describe( 'error handling', () => {
+
+		it( 'maps API errors', () => {
+			const result = { errors: [
+				{ code: 'error1', html: 'error one' },
+				{ code: 'error2', html: 'error two' },
+				{ code: 'error3' },
+			] };
+			const deferred = mockRejectedDeferred( 'error1', result, result );
+			const api: MwApi = {
+				...unusedApi,
+				assertCurrentUser: ( params: object ) => ( { assert: 'user', ...params } ),
+				postWithEditToken: jest.fn().mockReturnValue( deferred ),
+			};
+			const lexemeCreator = new MwApiLexemeCreator( api );
+
+			return expect( () => lexemeCreator.createLexeme(
+				'lemma',
+				'en',
+				'Q123',
+				'Q456',
+			) ).rejects.toStrictEqual( [
+				{ type: 'error1', message: 'error one' },
+				{ type: 'error2', message: 'error two' },
+				{ type: 'error3' },
+			] as SubmitError[] );
+		} );
+
+		it( 'handles HTTP errors', () => {
+			const deferred = mockRejectedDeferred( 'http', {
+				xhr: undefined,
+				textStatus: undefined,
+				exception: undefined,
+			} );
+			const api: MwApi = {
+				...unusedApi,
+				assertCurrentUser: ( params: object ) => ( { assert: 'user', ...params } ),
+				postWithEditToken: jest.fn().mockReturnValue( deferred ),
+			};
+			const lexemeCreator = new MwApiLexemeCreator( api );
+
+			return expect( () => lexemeCreator.createLexeme(
+				'lemma',
+				'en',
+				'Q123',
+				'Q456',
+			) ).rejects.toStrictEqual( [
+				{ type: 'http' },
+			] as SubmitError[] );
+		} );
+
 	} );
 
 } );
