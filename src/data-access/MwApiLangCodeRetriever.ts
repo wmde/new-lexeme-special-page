9ba20a716588ef234040da36a9d15b66f352b89c
Michael Große <mic.grosse@googlemail.com>
2022-04-08 11:36:02 +0200

    Add constructor validation

--- src/data-access/MwApiLangCodeRetriever.ts
+++ src/data-access/MwApiLangCodeRetriever.ts
@@ -4,7 +4,15 @@ import { processWbGetClaimsResponse, WbGetClaimsResponse } from './apiBaseLangCo
 
 export default class MwApiLangCodeRetriever implements LangCodeRetriever {
 
-	public constructor( private api: MwApi, private languageCodeProperty: string ) {
+	private readonly api: MwApi;
+	private readonly languageCodeProperty: string;
+
+	public constructor( api: MwApi, languageCodeProperty: string ) {
+		if ( typeof languageCodeProperty !== 'string' ) {
+			throw new Error( `Expected languageCodeProperty to be propertyId but received ${languageCodeProperty} instead!` );
+		}
+		this.api = api;
+		this.languageCodeProperty = languageCodeProperty;
 	}
 
 	public async getLanguageCodeFromItem( itemId: string ): Promise<string | null | false> {
