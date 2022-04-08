77bd3f0ebcb16ca80f2fb7fb158d2cc3781b8523
Michael Große <mic.grosse@googlemail.com>
2022-04-08 11:44:52 +0200

    Add a bit validation to guard against misconfigured property

--- src/data-access/apiBasedLangCodeRetriever.ts
+++ src/data-access/apiBasedLangCodeRetriever.ts
@@ -4,9 +4,13 @@ export type WbGetClaimsResponse = {
 	claims: StatementMap;
 };
 
-function getValueFromClaim( claim: Statement ): string | false {
+function getStringFromClaim( claim: Statement ): string | false {
 	if ( claim.mainsnak.snaktype !== 'value' ) {
 		return false;
+	}
+
+	if ( claim.mainsnak.datavalue?.type !== 'string' ) {
+		throw new Error( `Expected ${claim.mainsnak.property} to have DataValueType "string" but got "${claim.mainsnak.datavalue?.type}"!` );
 	}
 
 	return ( claim.mainsnak.datavalue as DataValue ).value;
@@ -22,12 +26,12 @@ export function processWbGetClaimsResponse(
 
 	const preferredRankStatements = response.claims[ languageCodeProperty ].filter( ( claim ) => claim.rank === 'preferred' );
 	if ( preferredRankStatements.length !== 0 ) {
-		return getValueFromClaim( preferredRankStatements[ 0 ] );
+		return getStringFromClaim( preferredRankStatements[ 0 ] );
 	}
 
 	const normalRankStatements = response.claims[ languageCodeProperty ].filter( ( claim ) => claim.rank === 'normal' );
 	if ( normalRankStatements.length !== 0 ) {
-		return getValueFromClaim( normalRankStatements[ 0 ] );
+		return getStringFromClaim( normalRankStatements[ 0 ] );
 	}
 
 	return null;
