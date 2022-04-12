f0b7e1e48de5569ab1104926998942cd0953bd67
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-04-12 17:32:00 +0200

    Rephrase comment to avoid overlong line (#133)
    I didn’t see the linter warning in 7494773f28.

--- src/data-access/LangCodeRetriever.ts
+++ src/data-access/LangCodeRetriever.ts
@@ -3,7 +3,7 @@ export default interface LangCodeRetriever {
 	/*
 	 * Returns the language code associated with the Item,
 	 * which may or may not be a valid language code, validation needs to be done by the caller.
-	 * Returns `null` if the Item does not have a language code associated with it (including a novalue statement).
+	 * Returns `null` if the Item has no associated language code (including a novalue statement).
 	 * Returns `false` if the Item has somevalue for the language code property.
 	 */
 	getLanguageCodeFromItem( itemId: string ): Promise<string | null | false>;
