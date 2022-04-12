a1447208631dea86c1eecce15810ee68dd38882d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-04-12 16:51:16 +0200

    Treat novalue like missing statement in LangCodeRetriever
    Somevalue should be treated like an error, but novalue can be considered
    equivalent to a missing statement for our purposes (i.e. shows no
    warning message).
    
    Bug: T305542

--- src/data-access/LangCodeRetriever.ts
+++ src/data-access/LangCodeRetriever.ts
@@ -3,8 +3,8 @@ export default interface LangCodeRetriever {
 	/*
 	 * Returns the language code associated with the Item,
 	 * which may or may not be a valid language code, validation needs to be done by the caller.
-	 * Returns `null` if the Item does not have a language code associated with it.
-	 * Returns `false` if the Item has novalue or somevalue for the language code property.
+	 * Returns `null` if the Item does not have a language code associated with it (including a novalue statement).
+	 * Returns `false` if the Item has somevalue for the language code property.
 	 */
 	getLanguageCodeFromItem( itemId: string ): Promise<string | null | false>;
 }
