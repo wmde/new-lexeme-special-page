1435ab7f28d31bc9a7ff8b3cf6391edaa82a7f89
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-04-27 16:38:22 +0200

    Remove unused mockSearchLinker
    Began life as an unusedSearchLinker, became renamed to mock when we
    realized it wouldn’t be unused after all in the App unit tests,
    eventually became wholly unused. Farewell.

--- tests/mocks/mockSearchLinker.ts
+++
@@ -1,9 +0,0 @@
-import SearchLinker from '@/plugins/SearchLinkerPlugin/SearchLinker';
-
-const mockSearchLinker: SearchLinker = {
-	getSearchUrlForLexeme: jest.fn().mockImplementation( () => {
-		throw new Error( 'SearchLinker should not be used in this test' );
-	} ),
-};
-
-export default mockSearchLinker;
