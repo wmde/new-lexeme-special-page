c60865ff9a3017a2a708ec491a5baaac068598a6
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-05-16 17:56:44 +0200

    Update Messages documentation
    Resolving an old FIXME.

--- src/plugins/MessagesPlugin/Messages.ts
+++ src/plugins/MessagesPlugin/Messages.ts
@@ -3,12 +3,12 @@ import MessageKeys from '@/plugins/MessagesPlugin/MessageKeys';
 import { inject, InjectionKey } from 'vue';
 
 /**
- * Usage (assuming this has been registered as a Vue plugin):
+ * Usage:
  *
- * FIXME: write something about when to use getText vs get
+ * const messages = useMessages();
  *
- * `this.$messages.get( 'wikibaselexeme-newlexeme-lemma' )`
- * `this.$messages.getText( 'wikibaselexeme-newlexeme-lemma' )`
+ * messages.get( 'wikibaselexeme-newlexeme-lemma' )
+ * messages.getUnescaped( 'wikibaselexeme-newlexeme-lemma' )
  */
 export default class Messages {
 
@@ -29,10 +29,27 @@ export default class Messages {
 		this.messagesRepository = messagesRepository;
 	}
 
+	/**
+	 * Parse the message and return it as HTML, with unsafe markup escaped as necessary.
+	 * Use this with v-html.
+	 *
+	 * @param {string} messageKey One of the well-known {@link MessageKeys}.
+	 * @param {...string|HTMLElement} params Parameters for the message
+	 * ($1, $2, ... in the message source).
+	 * @return {string} HTML
+	 */
 	public get( messageKey: MessageKeys, ...params: readonly ( string|HTMLElement )[] ): string {
 		return this.messagesRepository.get( messageKey, ...params );
 	}
 
+	/**
+	 * Get the message, with parameters replaced but without parsing or escaping anything.
+	 * Use this where the result will be escaped already, e.g. in {{}}.
+	 *
+	 * @param {string} messageKey One of the well-known {@link MessageKeys}.
+	 * @param {...string} params Parameters for the message ($1, $2, ... in the message source).
+	 * @return {string} plain text
+	 */
 	public getUnescaped( messageKey: MessageKeys, ...params: readonly string[] ): string {
 		return this.messagesRepository.getText( messageKey, ...params );
 	}
