915f0ed8316ccee1b3dd061f8b8391ba3d8edf08
Michael Große <michael.grosse@wikimedia.de>
2022-05-23 19:12:28 +0200

    Adjust Lemma input error test

--- tests/unit/components/LemmaInput.test.ts
+++ tests/unit/components/LemmaInput.test.ts
@@ -1,15 +1,33 @@
 import { DOMWrapper, mount } from '@vue/test-utils';
 import LemmaInput from '@/components/LemmaInput.vue';
 import { ConfigKey } from '@/plugins/ConfigPlugin/Config';
+import initStore from '@/store';
+import unusedLangCodeRetriever from '../../mocks/unusedLangCodeRetriever';
+import unusedLanguageCodesProvider from '../../mocks/unusedLanguageCodesProvider';
+import unusedTracker from '../../mocks/unusedTracker';
+import unusedLexemeCreator from '../../mocks/unusedLexemeCreator';
+import { nextTick } from 'vue';
+import RootState from '@/store/RootState';
+import { Store } from 'vuex';
 
 describe( 'LemmaInput', () => {
 
+	let store: Store<RootState>;
+
 	function createComponent( config: Record<string, unknown> = {} ) {
+		store = initStore( {
+			lexemeCreator: unusedLexemeCreator,
+			langCodeRetriever: unusedLangCodeRetriever,
+			languageCodesProvider: unusedLanguageCodesProvider,
+			tracker: unusedTracker,
+		} );
+
 		return mount( LemmaInput, {
 			props: {
 				modelValue: '',
 			},
 			global: {
+				plugins: [ store ],
 				provide: {
 					[ ConfigKey as symbol ]: { placeholderExampleData: {} },
 				},
@@ -31,16 +49,13 @@ describe( 'LemmaInput', () => {
 			expect( findInput( lemmaInputWrapper ).element.value ).toBe( testValue );
 		} );
 
-		it( ':error - displays an error message for value ERROR_LEMMA_TOO_LONG', () => {
-			const lemmaInputWrapper = createComponent( { props: { error: 'ERROR_LEMMA_TOO_LONG' } } );
+		it( ':error - displays an error message provided by the prop', async () => {
+			const lemmaInputWrapper = createComponent();
 
-			expect( lemmaInputWrapper.text() ).toContain( 'wikibaselexeme-newlexeme-error-lemma-is-too-long' );
-		} );
+			store.state.perFieldErrors.lemmaErrors.push( { messageKey: 'wikibaselexeme-newlexeme-error-no-lemma' } );
+			await nextTick();
 
-		it( ':error - displays an error message for value ERROR_NO_LEMMA', () => {
-			const lemmaInputWrapper = createComponent( { props: { error: 'ERROR_NO_LEMMA' } } );
-
-			expect( lemmaInputWrapper.text() ).toContain( 'wikibaselexeme-newlexeme-error-no-lemma' );
+			expect( lemmaInputWrapper.get( '.wikit-ValidationMessage--error' ).text() ).toContain( '⧼wikibaselexeme-newlexeme-error-no-lemma⧽' );
 		} );
 	} );
 
