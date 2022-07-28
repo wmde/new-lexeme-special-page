cd1279386ec90be10b9dc290174f1cd35498b737
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-07-28 17:28:15 +0200

    Show warning for anonymous users
    Using the standard Wikibase message, but without passing an argument
    into it. Special:NewLexeme passes the wikibase-entity-lexeme message
    into it (the argument used to be used as a sort of “your IP address will
    be visible in the history of the item/property” thing), but that message
    doesn’t actually exist; the $1 argument is also only used in one
    remaining translation (vec/Venetian), where I’ve asked the translator to
    update the translation so it doesn’t use $1 anymore. Once that’s done, I
    expect we’ll update Wikibase and WikibaseLexeme to also drop this
    message parameter.
    
    On the dev entry point, the warning can be seen by adding
    ?isAnonymous=true to the URL.
    
    Bug: T313466

---
+++ src/components/AnonymousEditWarning.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useConfig } from '@/plugins/ConfigPlugin/Config';
+import { useMessages } from '@/plugins/MessagesPlugin/Messages';
+import { computed } from 'vue';
+
+const messages = useMessages();
+const warning = computed(
+	() => messages.get( 'wikibase-anonymouseditwarning' ) );
+const config = useConfig();
+
+</script>
+
+<template>
+	<!-- eslint-disable vue/no-v-html -->
+	<p
+		v-if="config.isAnonymous"
+		class="wbl-snl-anonymous-edit-warning"
+		v-html="warning"
+	/>
+	<!-- eslint-enable -->
+</template>
+
+<style lang="scss" scoped>
+@import "@wmde/wikit-tokens/variables";
+@import "@wmde/wikit-vue-components/src/styles/mixins/Typography";
+
+.wbl-snl-anonymous-edit-warning {
+	@include body;
+}
+</style>
