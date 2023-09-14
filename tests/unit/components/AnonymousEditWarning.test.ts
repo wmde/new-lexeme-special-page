779e96c0c861a6dfd1ba150c66f601b72d667c21
Michael Große <michael.grosse@wikimedia.de>
2023-09-14 17:08:02 +0200

    Add anon edit warning link parameters (#517)
    The message mock for the snapshot test has changed so that also
    the arguments are in the snapshot.
    
    Bug: T330550

--- tests/unit/components/AnonymousEditWarning.test.ts
+++ tests/unit/components/AnonymousEditWarning.test.ts
@@ -2,37 +2,52 @@ import AnonymousEditWarning from '@/components/AnonymousEditWarning.vue';
 import { ConfigKey } from '@/plugins/ConfigPlugin/Config';
 import { MessagesKey } from '@/plugins/MessagesPlugin/Messages';
 import { mount } from '@vue/test-utils';
+import { AuthenticationLinkerKey } from '@/plugins/AuthenticationLinkerPlugin/AuthenticationLinker';
 
 describe( 'AnonymousEditWarning', () => {
 
 	it( 'does nothing if not anonymous', () => {
 		const messages = { get: jest.fn() };
+		const authenticationLinker = {
+			getLoginLink: jest.fn(),
+			getCreateAccountLink: jest.fn(),
+		};
 		const warning = mount( AnonymousEditWarning, {
 			global: {
 				provide: {
 					[ ConfigKey as symbol ]: { isAnonymous: false },
 					[ MessagesKey as symbol ]: messages,
+					[ AuthenticationLinkerKey as symbol ]: authenticationLinker,
 				},
 			},
 		} );
 
 		expect( warning.isVisible() ).toBe( false );
 		expect( messages.get ).not.toHaveBeenCalled();
+		expect( authenticationLinker.getLoginLink ).not.toHaveBeenCalled();
+		expect( authenticationLinker.getCreateAccountLink ).not.toHaveBeenCalled();
 	} );
 
 	it( 'matches snapshot if anonymous', () => {
-		const messages = { get: jest.fn().mockImplementation( ( key ) => key ) };
+		const messages = { get: jest.fn().mockImplementation( ( ...params ) =>
+			`(${params.join( ', ' )})`,
+		) };
+		const authenticationLinker = {
+			getLoginLink: jest.fn().mockReturnValue( 'loginLink' ),
+			getCreateAccountLink: jest.fn().mockReturnValue( 'createAccountLink' ),
+		};
 		const warning = mount( AnonymousEditWarning, {
 			global: {
 				provide: {
 					[ ConfigKey as symbol ]: { isAnonymous: true },
 					[ MessagesKey as symbol ]: messages,
+					[ AuthenticationLinkerKey as symbol ]: authenticationLinker,
 				},
 			},
 		} );
 
 		expect( warning.html() ).toMatchSnapshot();
-		expect( messages.get ).toHaveBeenCalledWith( 'wikibase-anonymouseditwarning' );
+		expect( messages.get ).toHaveBeenCalledWith( 'wikibase-anonymouseditwarning', 'loginLink', 'createAccountLink' );
 	} );
 
 } );
