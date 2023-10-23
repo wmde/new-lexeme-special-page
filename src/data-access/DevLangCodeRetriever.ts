96427e94cb2c831003ecb0bddabcd9232deb900f
Michael Große <mic.grosse@googlemail.com>
2023-10-23 15:57:03 +0200

    DevLangCodeRetriever: Adjust property-id used for language code
    This was changed in production to user the broader coverage provided by IETF code, see T348923.
    
    Bug: T348923

--- src/data-access/DevLangCodeRetriever.ts
+++ src/data-access/DevLangCodeRetriever.ts
@@ -6,7 +6,7 @@ export default class DevLangCodeRetriever implements LangCodeRetriever {
 		const response = await fetch( 'https://www.wikidata.org/w/api.php?' + new URLSearchParams( {
 			action: 'wbgetclaims',
 			entity: itemId,
-			property: 'P218',
+			property: 'P305',
 			props: '',
 			format: 'json',
 			origin: '*',
