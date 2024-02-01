eee24d7e0d2d8ebd66f4e0baa978df06788849c4
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-02-01 14:42:43 +0100

    Upgrade eslint and related dependencies (#633)
    The eslint upgrade has been blocked for a while because of
    unexpected interactions between eslint and other dependencies.
    
     - @rushstack/eslint-patch
       - documentation updates and typescript 5.3 support
     - @typescript-eslint/eslint-plugin and @typescript-eslint/parser
       - various bugfixes and updates to rules
     - @wmde/eslint-config-wikimedia-typescript
       - upgrade to the latest eslint-config-wikimedia
     - eslint-config-wikimedia
       - rule changes, more vue rules, more tests
     - eslint
       - too many fixes and updates to mention, but no breaking change
    
    .eslintrc.js has been extended with another ignore rule, and two
    tests were updated to comply with the new formatting rules.
    
    Bug: T341552

--- tests/mocks/unusedWikiRouter.ts
+++ tests/mocks/unusedWikiRouter.ts
@@ -1,7 +1,9 @@
 import WikiRouter from '@/plugins/WikiRouterPlugin/WikiRouter';
 
 const unusedWikiRouter: WikiRouter = {
-	goToTitle: jest.fn().mockImplementation( () => { throw new Error( 'WikiRouter should not be used in this test' ); } ),
+	goToTitle: jest.fn().mockImplementation( () => {
+		throw new Error( 'WikiRouter should not be used in this test' );
+	} ),
 };
 
 export default unusedWikiRouter;
